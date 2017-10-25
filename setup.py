71602b2a92a130cb1e17e7421be9814b5cb6c1d0
Jonathan <jonathan@saharacluster.com>
2017-10-25 11:27:13 +0300

    updated dir structure

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'ipdata',
   packages = ['ipdata'], # this must be the same as the name above
-  version = '1.0',
+  version = '1.1',
   description = 'Python Client for Ipdata.co - a Free Ip Geolocation API',
   author = 'Jonathan Kosgei',
   author_email = 'jonathan@ipdata.co',
   url = 'https://github.com/ipdata/python', # use the URL to the github repo
-  download_url = 'https://github.com/ipdata/python/archive/1.0.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/ipdata/python/archive/1.1.tar.gz', # I'll explain this in a second
   keywords = ['geolocation', 'ip geolocation', 'ip locate'], # arbitrary keywords
   classifiers = [],
 )
