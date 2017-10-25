4f53cc79e08210a17b80b8d88fe61ae581cba25c
Jonathan <jonathan@saharacluster.com>
2017-10-25 11:17:48 +0300

    Init

---
+++ ipdata.py
@@ -0,0 +1,19 @@
+import requests
+
+class ipdata:
+    base_url = 'https://api.ipdata.co/{ip}/{language}'
+    def __init__(self, api-key=None,language='en'):
+        self.apikey = apikey
+        self.language = language
+        self.headers = {'user-agent': 'ipdata-pypi/0.0.1'}
+    def lookup(self, ip):
+        try:
+            if self.apikey:
+                self.headers ['api-key'] = self.apikey
+            r = requests.get(self.base_url.format(ip=ip, language=self.language), headers=self.headers)
+            if r.status_code=='200':
+                return r.json()
+            return r.text
+        except requests.exceptions.RequestException as e:  # This is the correct syntax
+            print e
+            sys.exit(1)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ipdata',
+  packages = ['ipdata'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Python Client for Ipdata.co - a Free Ip Geolocation API',
+  author = 'Jonathan Kosgei',
+  author_email = 'jonathan@ipdata.co',
+  url = 'https://github.com/ipdata/python', # use the URL to the github repo
+  download_url = 'https://github.com/ipdata/python/archive/1.0.tar.gz', # I'll explain this in a second
+  keywords = ['geolocation', 'ip geolocation', 'ip locate'], # arbitrary keywords
+  classifiers = [],
+)
