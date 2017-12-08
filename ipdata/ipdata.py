cc144c34d0b48ca8fd608b58a62b1ff59900d0a8
Jonathan <jonathan@saharacluster.com>
2017-12-09 01:39:27 +0300

    version 2.6

--- ipdata/ipdata.py
+++ ipdata/ipdata.py
@@ -8,12 +8,10 @@ class ipdata:
         self.language = language
         self.headers = {'user-agent': 'ipdata-pypi/2.4'}
     def lookup(self, ip):
-        try:
-            if self.apikey:
-                self.headers ['api-key'] = self.apikey
-            r = requests.get(self.base_url.format(ip=ip, language=self.language), headers=self.headers)
-            if r.status_code==200:
-                return {'status':r.status_code, 'response':r.json()}
-            return {'status':r.status_code, 'response':r.text}
-        except requests.exceptions.RequestException as e:  # This is the correct syntax
-            print(e)
+        if self.apikey:
+            self.headers ['api-key'] = self.apikey
+        r = requests.get(self.base_url.format(ip=ip, language=self.language), headers=self.headers)
+        if r.status_code==200:
+            return {'status':r.status_code, 'response':r.json()}
+        return {'status':r.status_code, 'response':r.text}
+
