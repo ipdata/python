1b088ce6a04d13b1531157d84e4171f6c0ad57a7
Jonathan <jonathan@saharacluster.com>
2017-12-09 01:24:43 +0300

    version 2.5

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ def read(fname):
 setup(
   name = 'ipdata',
   packages = ['ipdata'], # this must be the same as the name above
-  version = '2.3',
+  version = '2.4',
   description = 'Python Client for Ipdata.co - a Free Ip Geolocation API',
   author = 'Jonathan Kosgei',
   author_email = 'jonathan@ipdata.co',
   url = 'https://github.com/ipdata/python', # use the URL to the github repo
-  download_url = 'https://github.com/ipdata/python/archive/2.3.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/ipdata/python/archive/2.4.tar.gz', # I'll explain this in a second
   keywords = ['geolocation', 'ip geolocation', 'ip locate'], # arbitrary keywords
   long_description=read('README'),
   classifiers = [],
