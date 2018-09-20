50611d70dae73974353f5b298d49ecaaa546d035
zoomequipd <github@zoomequipd.com>
2018-09-20 15:55:50 -0500

    use api-key as query parameter

--- ipdata/ipdata.py
+++ ipdata/ipdata.py
@@ -2,15 +2,13 @@
 import requests, json
 
 class ipdata:
-    base_url = 'https://api.ipdata.co/{ip}/{language}'
+    base_url = 'https://api.ipdata.co/{ip}/{language}/?api-key={apikey}'
     def __init__(self, apikey=None,language='en'):
         self.apikey = apikey
         self.language = language
         self.headers = {'user-agent': 'ipdata-pypi/2.4'}
     def lookup(self, ip):
-        if self.apikey:
-            self.headers ['api-key'] = self.apikey
-        r = requests.get(self.base_url.format(ip=ip, language=self.language), headers=self.headers)
+        r = requests.get(self.base_url.format(ip=ip, language=self.language, apikey=self.apikey), headers=self.headers)
         if r.status_code==200:
             return {'status':r.status_code, 'response':r.json()}
         return {'status':r.status_code, 'response':r.text}
