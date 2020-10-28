c30cc13b2780f22a9099ffe74cd139810b8d82b2
Stas Davydov <davidovsv@yandex.ru>
2020-10-28 23:10:53 +0300

    Implemented: myip, ip commands

--- ipdata/cli.py
+++ ipdata/cli.py
@@ -1,4 +1,5 @@
 import os
+import socket
 from pathlib import Path
 from sys import stderr
 
@@ -20,17 +21,31 @@ def get_api_key_path():
     return os.path.join(home, '.ipdata')
 
 
-@click.command()
-@click.argument('api_key', type=str)
-def init(api_key):
+def get_api_key():
     key_path = get_api_key_path()
-    override = False
     if os.path.exists(key_path):
         with open(key_path, 'r') as f:
             for line in f:
                 if line:
-                    override = True
-                break
+                    return line
+    else:
+        return None
+
+
+def get_my_ip():
+    hostname = socket.gethostname()
+    return socket.gethostbyname(hostname)
+
+
+@click.command()
+@click.argument('api_key', type=str)
+def init(api_key):
+    key_path = get_api_key_path()
+
+    override = False
+    existing_api_key = get_api_key()
+    if existing_api_key:
+        override = True
 
     ipdata = IPData(api_key)
     res = ipdata.lookup('8.8.8.8')
@@ -38,7 +53,7 @@ def init(api_key):
         with open(key_path, 'w') as f:
             f.write(api_key)
         if override:
-            print(f'Warning: You already have an IPData API Key "{line}" listed in {key_path}. '
+            print(f'Warning: You already have an IPData API Key "{existing_api_key}" listed in {key_path}. '
                   f'It will be overwritten with {api_key}',
                   file=stderr)
         print(f'New API Key is saved to {key_path}')
@@ -47,13 +62,45 @@ def init(api_key):
               file=stderr)
 
 
-@click.command(name='ipdata')
-def main():
-    pass
+@click.command()
+@click.option('--api_key', required=False, default=None, help='IPData API Key')
+def myip(api_key):
+    if api_key is None:
+        api_key = get_api_key()
+    if api_key is None:
+        print(f'Please specify IPData API Key', file=stderr)
+        return
+
+    ipdata = IPData(api_key)
+    ip = get_my_ip()
+    try:
+        res = ipdata.lookup(ip)
+        print(res)
+    except ValueError as e:
+        print(f'Error: Your IP address {e}', file=stderr)
+
+
+@click.command()
+@click.argument('ip', type=str)
+@click.option('--api_key', required=False, default=None, help='IPData API Key')
+def ip(ip, api_key):
+    if api_key is None:
+        api_key = get_api_key()
+    if api_key is None:
+        print(f'Please specify IPData API Key', file=stderr)
+        return
+
+    ipdata = IPData(api_key)
+    try:
+        res = ipdata.lookup(ip)
+        print(res)
+    except ValueError as e:
+        print(f'Error: IP address {e}', file=stderr)
 
 
 cli.add_command(init)
-# cli.add_command(main)
+cli.add_command(myip)
+cli.add_command(ip)
 
 
 if __name__ == '__main__':
