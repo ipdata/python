46d987d7461bc0fc851353aff93eae69d6934cf4
Stas Davydov <davidovsv@yandex.ru>
2021-02-08 02:39:22 +0300

    Optimize imports in unit-tests

--- ipdata/test_cli.py
+++ ipdata/test_cli.py
@@ -1,11 +1,11 @@
 import multiprocessing
-from io import StringIO
 import sys
 import unittest
+from io import StringIO
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 
-from ipdata.cli import json_filter, todo, ip, _batch, get_json_value
+from cli import json_filter, todo, _batch, get_json_value
 
 
 class CliTestCase(TestCase):
@@ -26,35 +26,39 @@ class CliTestCase(TestCase):
 
 
 class CliTodoTestCase(TestCase):
-    def test_todo_call_with_ip_address(self):
-        with patch.object(sys, 'argv', ['__nope__', '1.1.1.1']) as m1, \
-                patch('ipdata.cli.ip') as m2, \
-                patch('ipdata.cli.cli') as m3:
+    @staticmethod
+    def test_todo_call_with_ip_address():
+        with patch.object(sys, 'argv', ['__nope__', '1.1.1.1']), \
+                patch('cli.ip') as m2, \
+                patch('cli.cli') as m3:
             todo()
             m3.assert_not_called()
             m2.assert_called_once()
 
-    def test_todo_call_with_param(self):
-        with patch.object(sys, 'argv', ['__nope__', 'abc']) as m1, \
-                patch('ipdata.cli.ip') as m2, \
-                patch('ipdata.cli.cli') as m3:
+    @staticmethod
+    def test_todo_call_with_param():
+        with patch.object(sys, 'argv', ['__nope__', 'abc']), \
+                patch('cli.ip') as m2, \
+                patch('cli.cli') as m3:
             todo()
             m2.assert_not_called()
             m3.assert_called_once()
 
-    def test_todo_call_without_params(self):
-        with patch.object(sys, 'argv', ['__nope__']) as m1, \
-                patch('ipdata.cli.ip') as m2, \
-                patch('ipdata.cli.cli') as m3:
+    @staticmethod
+    def test_todo_call_without_params():
+        with patch.object(sys, 'argv', ['__nope__']), \
+                patch('cli.ip') as m2, \
+                patch('cli.cli') as m3:
             todo()
             m2.assert_not_called()
             m3.assert_called_once()
 
 
 class CliInitTestCase(TestCase):
-    def test_init_noargs(self):
-        with patch.object(sys, 'argv', ['__nope__', 'init']) as m1, \
-                patch('ipdata.cli.init') as m2, \
+    @staticmethod
+    def test_init_noargs():
+        with patch.object(sys, 'argv', ['__nope__', 'init']), \
+                patch('cli.init') as m2, \
                 patch('sys.exit') as m3:
             todo()
             m2.assert_not_called()
@@ -100,7 +104,8 @@ class BatchTestCase(TestCase):
         expected = {'a': 1, 'e': [4, 5]}
         self.assertDictEqual(expected, res)
 
-        self.assertRaises(ValueError, lambda: json_filter([{'a': 1, 'b': 1}, {'a': 2, 'b': 2}, {'a': 3, 'b': 3}], ['m.a', 'm.b']))
+        self.assertRaises(ValueError, lambda: json_filter([
+            {'a': 1, 'b': 1}, {'a': 2, 'b': 2}, {'a': 3, 'b': 3}], ['m.a', 'm.b']))
 
     def test_get_json_value(self):
         json = {'ip': 1, 'languages': ['English', 'Russian']}
