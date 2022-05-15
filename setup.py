6ca02ad518fadae9686d8db0e88bb261033e6607
Jonathan Kosgei <jonathan@ipdata.co>
2022-05-15 23:10:01 +0300

    update python version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=["ipdata"],
     include_package_data=True,
